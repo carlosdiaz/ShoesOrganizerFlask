25848b96b59845a38978cda28916db4320c761f7
carlosdiaz <alberto.carlos@gmail.com>
2015-06-17 09:16:42 -0500

    Adding welcome method

--- shoesapp.py
+++ shoesapp.py
@@ -34,3 +34,16 @@ def welcome():
 	shoes = [dict(name=row[0], description=row[1], model=row[2] , image=row[3]) for row in cur.fetchall()]
 	g.db.close()
 	return render_template('welcome.html', shoes = shoes)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	error= None
+	if request.method == 'POST':
+		if request.form['username'] != 'admin' or request.form['password'] != 'admin':	
+			error='Intente nuevamente'
+		else:
+			session['logged_in'] = True
+			flash('Has iniciado sesion')
+			return redirect(url_for('welcome'))
+	return render_template('login.html', error=error)
